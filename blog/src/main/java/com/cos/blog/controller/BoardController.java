3a59f7f3749f1e93eb9f15d67a7476f145f18f21
YongJinPark91 <skylock45@naver.com>
2023-12-12 18:13:50 +0900

    feat SpringBoot Day15
    feat SpringBoot Day15

--- blog/src/main/java/com/cos/blog/controller/BoardController.java
+++ blog/src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,19 +1,36 @@
 package com.cos.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.model.User;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping({"","/"})
-	public String index() { // 컨트롤러에서 세션을 어떻게 찾는지?
+	public String index(Model model,@PageableDefault(size=3, sort="id", direction = Direction.DESC) Pageable pageable) { // 컨트롤러에서 세션을 어떻게 찾는지?
 		// /WEB-INF/views/index.jsp
-		return "index";
+		
+		/*
+		 * 메인페이지에서 데이터를 보기 위해서는 Model이 필요하다.
+		 *    "/"를 요청하면 boards의 이름으로 글목록을 모두 가져온다. 
+		 *    가져오는 방법은 boardrespoitory의 findall이 모두 갖고 있기 때문에 모든 데이터를 가져올 수 있다.
+		 */
+		model.addAttribute("boards", boardService.글목록(pageable));
+		return "index"; // viewResolver 작동된다.
 	}
 	
 	@GetMapping("/board/saveForm")
--- blog/src/main/java/com/cos/blog/service/BoardService.java
+++ blog/src/main/java/com/cos/blog/service/BoardService.java
@@ -1,7 +1,11 @@
 package com.cos.blog.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +27,15 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+	
+	public Page<Board> 글목록(Pageable pageable){
+		return boardRepository.findAll(pageable);
+		/*
+		 * 페이징 처리를 하기 위해서는 Controller에서 Pageable을 인자로 받아오게 되고,
+		 * 그 값을 Service에서도 마찬가지로 인자로 받아와 findAll로 pageable을 넘기면 페이징처리가 된다.
+		 * 그렇지만 최초 List<Board>형태에서 => Page<Board> 형태로 변환되기 때문에 자료형을 변환해주어야 한다.
+		 */
 	}
 
 }
